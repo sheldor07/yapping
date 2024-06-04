67727e8af47ffffe5e8e572faa79a4dd1136fbc4
Yajat Gulati <yajatgulati01@gmail.com>
2024-06-04 11:42:49 +0530

    feat: added form validation and error message && bug: fixed utf-8 emoji error
    Signed-off-by: Yajat Gulati <yajatgulati01@gmail.com>

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const apiMessage = document.getElementById("apiMessage");
   const changeBtn = document.getElementById("changeBtn");
   const formContainer = document.getElementById("formContainer");
-  const message = document.getElementById("message");
+  const invalidApiKey = document.getElementById("invalidApiKey");
 
   chrome.storage.sync.get("OPENAI_API_KEY", function (result) {
     if (result.OPENAI_API_KEY) {
@@ -16,16 +16,28 @@ document.addEventListener("DOMContentLoaded", function () {
   // Save button click event
   saveBtn.addEventListener("click", function () {
     const apiKey = apiKeyInput.value.trim();
+    if (!apiKey) {
+      apiKeyInput.classList.add("invalid");
+      return;
+    }
+    if (apiKey.substring(0, 3) !== "sk-") {
+      invalidApiKey.classList.remove("hidden");
+      return;
+    }
     if (apiKey) {
       chrome.storage.sync.set({ OPENAI_API_KEY: apiKey });
+      apiKeyInput.classList.remove("invalid");
+      invalidApiKey.classList.add("hidden");
       apiMessage.classList.remove("hidden");
       changeBtn.classList.remove("hidden");
       saveBtn.classList.add("hidden");
+      apiKeyInput.classList.add("hidden");
     }
   });
 
   // Change button click event
   changeBtn.addEventListener("click", function () {
+    apiKeyInput.classList.remove("hidden");
     apiMessage.classList.add("hidden");
     changeBtn.classList.add("hidden");
     saveBtn.classList.remove("hidden");
